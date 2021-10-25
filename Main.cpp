57ff96aadf60a880d5549d98d773de1ce3abdedb
andrewmes161 <79704821+andrewmes161@users.noreply.github.com>
2021-10-25 03:09:11 -0400

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,100 @@
+
+#include"Model.h"
+
+
+const unsigned int width = 1600;
+const unsigned int height = 1600;
+
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a GLFWwindow object of 1600 by 1600 pixels
+	GLFWwindow* window = glfwCreateWindow(width, height, "Andrew Messier Final Project", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, width, height);
+
+	// Generates Shader object using shaders default.vert and default.frag
+	Shader shaderProgram("default.vert", "default.frag");
+
+	// Take care of all the light related things
+	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
+
+	shaderProgram.Activate();
+	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+
+	// Enables the Depth Buffer
+	glEnable(GL_DEPTH_TEST);
+
+	// Creates camera object
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
+	// Gets a Model...
+	// I personally thought that building out a model through shapes isn't a good representation of skills for portfolio item for this class.
+	// In real world applications, models would be created using 3D software like Autodesk Maya, and not be hard programmed in openGL.
+	// Then these models would be imported into a 3D engine for viewing. So I found some online tutorials that take an existing
+	// mesh scene using .gltf format. Which is the recommend standard format by Khronos, the makers of openGL. This allowed me
+	// to import a high resolution mesh and textures, and view it in my small graphics window. I found 2 free models online that
+	// followed the format of the tutorial, and placed them in the project folder.
+
+
+	Model model("models/map/scene.gltf"); // displays a Map in the scene
+	//Model model("models/grindstone/scene.gltf"); // displays a grindstone in the scene think that it needs to be rotated 90 degrees...
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Specify the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and depth buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Handles camera inputs
+		camera.Inputs(window);
+		// Updates and exports the camera matrix to the Vertex Shader
+		camera.updateMatrix(100.0f, 1.0f, 100.0f);
+
+		// Draw a model
+		model.Draw(shaderProgram, camera);
+
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+	// Delete all the objects we've created
+	shaderProgram.Delete();
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
+}
